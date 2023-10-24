80c9be8c39316456540eac39c7ecea4fdb063984
허성은 <aysel@MacBook-Pro.local>
2023-10-25 00:08:06 +0900

    make demo App using SwiftUI

---
+++ L1 Demo/L1 Demo/L1_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L1_DemoApp.swift
+//  L1 Demo
+//
+//  Created by 허성은 on 2023/10/24.
+//
+
+import SwiftUI
+
+@main
+struct L1_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
